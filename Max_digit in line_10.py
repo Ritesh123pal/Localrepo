9318b9b153da1373ee9b8fb72569242f2dde6c7b
Ritesh Pal <palr44241@gmail.com>
2026-08-24 00:11:58 +0530

    Added program to print the line have max num of digit character in a file

---
+++ Max_digit in line_10.py
@@ -0,0 +1,13 @@
+# return the line containing maximum nunber of digit characterin file.txt
+max_digit=0
+result_line=" "
+with open("file.txt","r")as f:
+   for line in f:
+      digit_count=0
+      for char in line:
+         if(char.isdigit ()):
+            digit_count+=1
+         if(digit_count>=max_digit):
+            max_digit=digit_count
+            result_line=line   
+print(result_line)            
